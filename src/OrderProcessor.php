d01e1a9bda321d664c62d42f5bf3047ef40ef22d
Zeyad Gamal <99384081+Zeyad-Gamal@users.noreply.github.com>
2026-05-17 14:04:53 +0300

    Update OrderProcessor.php

--- src/OrderProcessor.php
+++ src/OrderProcessor.php
@@ -1,36 +1,21 @@
  <?php
+
 namespace App;
+
 class OrderProcessor {
+    public function process($name, $service) {
+        if (empty($name) || strlen($name) < 3) {
+            return ["status" => "error", "message" => "Invalid Name"];
+        }
+        
+        $validServices = ['Web Development', 'DevOps Automation', 'Cloud Migration'];
+        if (!in_array($service, $validServices)) {
+            return ["status" => "error", "message" => "Invalid Service"];
+        }
 
-    public function process() {
-        $a = 1;
-        $b = 2;
-        $c = 3;
-        $d = 4;
-        $e = 5;
-        $f = 6;
-
-        return $a;
+        return [
+            "status" => "success",
+            "message" => "Order Confirmed for $name"
+        ];
     }
 }
-
-
-// namespace App;
-
-// class OrderProcessor {
-//     public function process($name, $service) {
-//         if (empty($name) || strlen($name) < 3) {
-//             return ["status" => "error", "message" => "Invalid Name"];
-//         }
-        
-//         $validServices = ['Web Development', 'DevOps Automation', 'Cloud Migration'];
-//         if (!in_array($service, $validServices)) {
-//             return ["status" => "error", "message" => "Invalid Service"];
-//         }
-
-//         return [
-//             "status" => "success",
-//             "message" => "Order Confirmed for $name"
-//         ];
-//     }
-// }
